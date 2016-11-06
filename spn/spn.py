bdeb5b07639d47a6b4a75c9f428a4035df2da37a
whsu <whsu@users.noreply.github.com>
2016-11-06 10:38:50 -0500

    Raise error on invalid parameters

--- spn/spn.py
+++ spn/spn.py
@@ -6,10 +6,33 @@ from .normal_leaf_node import NormalLeafNode
 from .multi_normal_leaf_node import MultiNormalLeafNode
 
 class SPNParams:
-	def __init__(self, batchsize=128, mergebatch=128, corrthresh=0.1, equalweight=True,
-	             updatestruct=True, maxdepth=20, prunebatch=1000, mvleaf=True,
-	             mvmaxscope=2, leaftype="normal"):
-		assert not (mvmaxscope > 2 and leaftype=="binary")
+	"""
+	Parameters
+	----------
+	batchsize : number of samples in a mini-batch.
+	mergebatch : number of samples a product node needs to see before updating
+	             its structure.
+	corrthresh : correlation coefficient threshold above which two variables
+	             are considered correlated.
+	equalweight : whether sum nodes should consider children as having equal
+	              weights when deciding which children to pass data to.
+	updatestruct : whether to update the network structure.
+	maxdepth : depth at which to prune the tree.
+	prunebatch : number of samples between each pruning.
+	mvleaf : whether to use multivariate leaves.
+	mvmaxscope : number of variables that can be combined into a multivariate
+	             leaf node.
+	leaftype : type of leaf nodes, one of "normal", "binary", "binarynormal".
+	"""
+	def __init__(self, batchsize=128, mergebatch=128, corrthresh=0.1,
+	             equalweight=True, updatestruct=True, maxdepth=20,
+	             prunebatch=1000, mvleaf=True, mvmaxscope=2, leaftype="normal"):
+		if leaftype=="binary" and mvmaxscope > 2:
+			raise ValueError("Binary leaf nodes cannot have more than two "
+			                 "variables in scope.")
+		if (leaftype=="binary" or leaftype=="binarynormal") and batchsize > 1:
+			raise ValueError("Batch size greater than 1 not implemented "
+			                 "for binary data.")
 		self.batchsize = batchsize
 		self.mergebatch = mergebatch
 		self.corrthresh = corrthresh
@@ -23,6 +46,15 @@ class SPNParams:
 		self.binary = False if leaftype=="normal" else True
 
 class SPN:
+	"""
+	Parameters
+	----------
+	node : int or Node
+		if int, number of variables
+		if Node, root of network
+	params : SPNParams
+		parameters of the network
+	"""
 	def __init__(self, node, params):
 		if type(node) == int:
 			numvar = node
